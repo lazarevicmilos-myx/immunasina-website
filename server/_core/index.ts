3bcfc42d48dcf544f4e275605a135e385dc2ce46
Milos Lazarevic <lazarevic.milos.a3m@gmail.com>
2026-07-03 21:13:57 +0200

    fix import error

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -7,7 +7,7 @@ import { registerOAuthRoutes } from "./oauth";
 import { registerStorageProxy } from "./storageProxy";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
-//import { serveStatic, setupVite } from "./vite";
+import { serveStatic, setupVite } from "./vite";
 
 function isPortAvailable(port: number): Promise<boolean> {
   return new Promise(resolve => {
