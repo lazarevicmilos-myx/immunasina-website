032e9e707ccf7bca2859fa699b415796c9bad255
Milos Lazarevic <lazarevic.milos.a3m@gmail.com>
2026-07-17 14:52:49 +0200

    Wrap root with suspense block

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
+import { Suspense } from "react";
 import superjson from "superjson";
 import App from "./App";
 import { getLoginUrl } from "./const";
@@ -54,11 +55,13 @@ const trpcClient = trpc.createClient({
 });
 
 createRoot(document.getElementById("root")!).render(
-  <trpc.Provider client={trpcClient} queryClient={queryClient}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </trpc.Provider>
+  <Suspense fallback={<div>Loading...</div>}>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </trpc.Provider>
+  </Suspense>
 );
 
 // Suppress i18next warning about missing backend
